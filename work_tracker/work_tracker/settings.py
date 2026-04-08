4502242e8728aa1f57d22fe2cd546afbd205e853
jc <jcmpes@gmail.com>
2026-04-08 22:47:15 +0200

    feat(settings): added CSRF_TRUSTED_ORIGINS

--- work_tracker/work_tracker/settings.py
+++ work_tracker/work_tracker/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 # Application definition
 INSTALLED_APPS = [
